a3cf12a30c3d32d4366d62ae057de337e3d148aa
aicha-manel <chf.aimanel@gmail.com>
2025-05-06 10:52:34 +0100

    Derniers changements dans prediction

--- src/pages/prediction.jsx
+++ src/pages/prediction.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
 
-import PredBar from '../components/predComponent/predBar';
+import React, { useState } from 'react';
+
+
 import NewPredictionButton from '../components/predComponent/newPredictionButton';
 import GrapheSection from '../components/predComponent/grapheSection';
-import GrapheAction from '../components/predComponent/grapheAction';
+
 import MethodSummary from '../components/predComponent/methodSummary';
-import PredictionResults from '../components/predComponent/predictionResults';
+
 import CustomSidebar from '../components/CustomSidebar';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 
 const Prediction = () => {
+  const [selectedData, setSelectedData] = useState(null);
   return (
     <div className="flex h-screen">
+       {/* <GraphProvider> */}
       {/* Sidebar fixe à gauche */}
         <ProSidebarProvider>
               <CustomSidebar />
@@ -19,20 +22,25 @@ const Prediction = () => {
 
       {/* Contenu principal prend le reste */}
       <div className="flex-1 p-8   overflow-auto bg-neutral-200">
-        <div className="flex justify-between items-center mb-6 rounded  p-3  bg-white shadow">
+        {/* <div className="flex justify-between items-center mb-6 rounded  p-3  bg-white shadow">
           <h5 className="text-lg font-bold text-gray-600 ">Prediction</h5>
           <h5 className="text-lg font-bold text-gray-600 ">Nom de l'entreprise</h5>
-        </div>
+        </div> */}
 
-       <PredBar />
-    <NewPredictionButton/>  
-    <GrapheSection/>
-    <GrapheAction/>
+      
+<GrapheSection data={selectedData} />
+
     <MethodSummary/>
-    <PredictionResults/>
+    <NewPredictionButton/>  
+
+  
+
       </div>
+      {/* </GraphProvider> */}
     </div>
   );
 };
+
+
 
 export default Prediction;
